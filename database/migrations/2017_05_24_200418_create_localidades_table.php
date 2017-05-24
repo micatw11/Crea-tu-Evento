1e3865555b93db400c9d88fd5a11704efc8c9c35
cmcuriqueo <cmcuriqueo@udc.edu.ar>
2017-05-24 20:51:15 -0300

    Provincias-Localidades migration-seeder

---
+++ database/migrations/2017_05_24_200418_create_localidades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocalidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('localidades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_privincia');
+            $table->string('nombre', 70);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('localidades');
+    }
+}
