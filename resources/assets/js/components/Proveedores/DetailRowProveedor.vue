332fc40e1c6658afbe17a0b2b6dd179beb79c317
cmcuriqueo <cmcuriqueo@udc.edu.ar>
2017-06-29 19:27:06 -0300

    se agrega carpeta de proveedores para documentacion y se agrega informacion a las columnas de proveedores

--- resources/assets/js/components/Proveedores/DetailRowProveedor.vue
+++ resources/assets/js/components/Proveedores/DetailRowProveedor.vue
@@ -3,27 +3,48 @@
         <div class="col-sm-3">
             <img :src="'/storage/avatars/'+ rowData.user.usuario.avatar" width="150" height="150" class="img-responsive">
         </div>
-        <div class="col-sm-9">
-          <p class="inline-block">
-            <label>Nombre: </label>
-            <span>{{rowData.nombre}}</span>
-          </p>
-          <p class="inline-block">
-            <label>Email: </label>
-            <span>{{rowData.email}}</span>
-          </p>
-          <p class="inline-block">
-            <label>N° de Cuit: </label>
-            <span>{{rowData.cuit}}</span>
-          </p>
-          <p class="inline-block">
-            <label>Estado: </label>
-            <span>{{rowData.estado}}</span>
-          </p>
-          <p class="inline-block">
-            <label>Genero: </label>
-            <span>{{rowData.user.usuario.sexo == 'F' ? 'Femenino' : 'Masculino'}}</span>
-          </p>
+        <div class="col-sm-5">
+            <p class="inline-block">
+                <label>Nombre: </label>
+                <span>{{rowData.nombre}}</span>
+            </p>
+            <p class="inline-block">
+                <label>Email: </label>
+                <span>{{rowData.email}}</span>
+            </p>
+            <p class="inline-block">
+                <label>N° de Cuit: </label>
+                <span>{{rowData.cuit}}</span>
+            </p>
+            <p class="inline-block">
+                <label>Estado: </label>
+                <span>{{rowData.estado}}</span>
+            </p>
+            <p class="inline-block">
+                <label>IIBB: </label>
+                <span>{{rowData.ingresos_brutos}}</span>
+            </p>
+            <p class="inline-block">
+                <label>DNI: </label>
+                <a :href="'/storage/proveedores/'+ rowData.dni">
+                    {{rowData.dni}}
+                </a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <p><label>DATOS DE USUARIO</label></p>
+            <p class="inline-block">
+                <label>Nombre: </label>
+                <span>{{rowData.user.usuario.nombre}} {{rowData.user.usuario.apellido}}</span>
+            </p>
+            <p class="inline-block">
+                <label>Email: </label>
+                <span>{{rowData.user.email}}</span>
+            </p>
+            <p class="inline-block">
+                <label>Genero: </label>
+                <span>{{rowData.user.usuario.sexo == 'F' ? 'Femenino' : 'Masculino'}}</span>
+            </p>
         </div>
     </div>
 </template>
