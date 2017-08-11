aa528614837a46efc155fb4de3578e20d5ed554c
micatw11 <micatw11@hotmail.com>
2017-08-11 16:01:41 -0300

    modificaciones varias merge

---
+++ resources/assets/js/components/Proveedores/Publicaciones/Perfil.vue
@@ -0,0 +1,94 @@
+<template>
+	<div class="box">
+	            
+        	<div class="col-sm-12">
+            	<div class="col-sm-12">
+	                <div class="col-sm-8">
+	                    <p>
+	                        <h3>{{item.titulo}}</h3>
+	                    </p>
+	                   	<p>
+	                        {{item.descripcion}}
+	                    </p>
+	                </div>
+	                <div class="col-sm-4" style="text-align: center;">
+	                    <button type="button" class="btn-block" @click="modificar(item.id)" >Modificar</button>
+	                    <button type="button" class="btn-block" @click="baja(item.id)" >Eliminar</button>
+	                </div>
+	            </div>
+	            <div class="col-sm-12">
+
+                	<div class="col-sm-2" v-for="img in item.fotos">
+                    	<img 
+                    		:src="'/storage/proveedores/publicaciones/'+img.nombre" 
+                    		class="img-responsive" 
+                    		style="max-width:130px;">
+                    </div>
+                </div>
+            </div>
+	        
+	        <!--</div>-->
+	        <hr>
+	
+	</div>
+</template>
+<script>
+	import route from './../../../routes.js'
+	import auth from './../../../auth.js'
+	export default {
+		props: {
+			proveedorId: {
+				required: true
+			}
+		},
+		data() {
+			return {
+				publicaciones: [],
+				productoId: null,
+				listPath : [{route: '/', name: 'Home'}, {route: '/usuario/'+auth.user.profile.id +'/perfil', name: 'Perfil'}],
+				auth: auth
+			}
+		},
+		mounted(){
+			this.getProductos();
+		},
+		methods: {
+			getProductos: function(){
+	            this.$http.get('api/proveedor/'+this.proveedorId+'/publicacion' )
+	                .then(response => {
+	                    this.publicaciones = response.data.publicaciones
+	                }, response => {
+	                    this.$toast.error({
+	                        title:'¡Error!',
+	                        message:'No se han podido cargar las publicaciones. :('
+	                    });
+
+	                })
+			},
+			modificar(id){
+				this.$events.fire('changePath', this.listPath, 'Modificar Publicacion');
+				route.push('/publicacion/'+id+'/edit');
+			},
+			baja(id){
+	            this.$http.delete('api/publicacion/'+id )
+	                .then(response => {
+	                    this.$toast.success({
+	                        title:'Acción realizada!',
+	                        message:'La acción se han realizado correctamente. :D'
+	                    });
+	                    this.getProductos()
+	                }, response => {
+	                    this.$toast.error({
+	                        title:'¡Error!',
+	                        message:'No se han podido realizar la acción. :('
+	                    });
+
+	                })
+			},
+			goToNewPublicacion(){
+				this.$events.fire('changePath', this.listPath, 'Nueva Publicacion');
+				route.push('/publicacion/new');
+			}
+		}
+	}
+</script>
